4376155b68ee609f1b78606a36205b47b035ef0e
Olajumoke <rukkylaw.tl@gmail.com>
2022-08-12 12:42:31 +0100

    handling mongoose errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,16 @@
 
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
+  // console.log(err)
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message ||  'Something went wrong, try again later'
   };
+  if (err.name === 'ValidationError') customError.statusCode = StatusCodes.BAD_REQUEST
+  if (err.name === "CastError") {
+    customError.msg = `No ID with ${err.value} found`
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
   if (err.code && err.code ===11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
     err.statusCode = 400
